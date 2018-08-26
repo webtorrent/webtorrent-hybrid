2d9d7f9849aa2389426b35f94b958a3dd0edb6b1
Zsolt Németh <hgghyxo@gmail.com>
2018-08-26 14:44:16 +0200

    refactored to es6 arrow functions

--- examples/node-seed.js
+++ examples/node-seed.js
@@ -7,7 +7,7 @@ var filePath = './node-seed.js'
 
 console.log('filePath:', filePath)
 
-client.seed(filePath, function (torrent) {
+client.seed(filePath, torrent => {
   console.log('torrentId (info hash):', torrent.infoHash)
   // console.log('torrentId (magnet link):', torrent.magnetURI)
 })
