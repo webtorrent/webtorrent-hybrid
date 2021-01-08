327d759030b63f5989542579a5ba96392471a5e4
Sarthak Bhagwat <58619383+s-bhagwat@users.noreply.github.com>
2021-01-08 08:14:49 -0500

    Downloading files with folder structure
    It is a example for downloading torrent files with folder structure same as the torrent.
    It is using file.path for getting path of a file then simply create that directories using fs node module
    for this I've used two promises one for checking the directory is exist or not another for creating it recursively.

---
+++ examples/downloadWithDirectory.js
@@ -0,0 +1,80 @@
+let WebTorrent = require("webtorrent-hybrid/index");
+let fs = require("fs");
+const path = require("path");
+
+let client = new WebTorrent();
+let file;
+const torrentUrl =
+  "magnet:?xt=urn:btih:9cfa12f0ae191bda5434d9755d99786e40a0c768&dn=%5bFreeCourseSite.com%5d%20Udemy%20-%20React%20-%20The%20Complete%20Guide%20(incl%20Hooks%2c%20React%20Router%2c%20Redux)&tr=udp%3a%2f%2ftracker.coppersurfer.tk%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.leechers-paradise.org%3a6969%2fannounce&tr=udp%3a%2f%2f9.rarbg.to%3a2710%2fannounce&tr=udp%3a%2f%2fexodus.desync.com%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.uw0.xyz%3a6969%2fannounce&tr=udp%3a%2f%2fopen.stealth.si%3a80%2fannounce&tr=udp%3a%2f%2ftracker.tiny-vps.com%3a6969%2fannounce&tr=udp%3a%2f%2fopen.demonii.si%3a1337%2fannounc4&tr=udp%3a%2f%2fpublic-tracker.zooki.xyz%3a6969%2fannounce&tr=udp%3a%2f%2fforever.publictracker.xyz%3a6969%2fannounce&tr=udp%3a%2f%2ftracker.cyberia.is%3a6969%2fannounce&tr=udp%3a%2f%2fapi.bitumconference.ru%3a6969%2fannounce&tr=udp%3a%2f%2fdiscord.heihachi.pw%3a6969%2fannounce&tr=udp%3a%2f%2ffe.dealclub.de%3a6969%2fannounce&tr=udp%3a%2f%2fgra1.joshkeegan.co.uk%3a6969%2fannounce";
+
+const createDirectoryPro = (directory) => {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(directory, { recursive: true }, (err) => {
+      if (err) {
+        console.log("Error is happend in creation of directory");
+        reject("Could not make directory");
+      }
+      resolve("Successfully created directory");
+    });
+  });
+};
+
+const checkDirectoryPro = (directory) => {
+  return new Promise((resolve, reject) => {
+    fs.access(directory, async (err) => {
+      if (err) {
+        console.log("Creating directory because directory is not exist");
+        //directory doesn't exist so create it and then start downloading
+        try {
+          let dirmsg = await createDirectoryPro(directory);
+          resolve(dirmsg);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      resolve("Dirctory already exist.");
+    });
+  });
+};
+
+let videoTorrent = client.add(
+  torrentUrl,
+  { announce: ["wss://tracker.openwebtorrent.com"] },
+  (torrent) => {
+    try {
+      // Stream each file to the disk
+      const files = torrent.files;
+      let length = files.length;
+      files.forEach(async (file) => {
+        let directory = file.path.replace(file.name, "");
+        try {
+          let directoryMsg = await checkDirectoryPro(directory);
+          console.log(directoryMsg);
+        } catch (err) {
+          console.log(err);
+        }
+        console.log("file:\t\t" + file.name);
+        const source = file.createReadStream();
+        const destination = fs.createWriteStream(file.path);
+
+        source
+          .on("end", () => {
+            console.log("file:\t\t", file.name);
+            // close after all files are saved
+            length -= 1;
+            console.log("download remining = " + length);
+            if (!length) process.exit();
+          })
+          .pipe(destination);
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+);
+//for catching errors
+videoTorrent.on("error", (err) => {
+  if (err) {
+    console.log("torrent error-", err);
+  }
+});
